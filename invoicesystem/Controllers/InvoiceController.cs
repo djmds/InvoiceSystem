95a1344d14f5c03149e92f99c336f811933ab97d
deepak jain <dj.mandsaur@gmail.com>
2022-04-05 11:22:33 +0530

    v1

---
+++ invoicesystem/Controllers/InvoiceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace invoicesystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InvoiceController : ControllerBase
+    {
+       
+
+        private readonly ILogger<InvoiceController> _logger;
+
+        public InvoiceController(ILogger<InvoiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+            
+        //}
+    }
+}
